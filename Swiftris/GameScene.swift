9b876c815c13b7b31a78871de9b49f05b7ae2e6b
Stuti Misra <smisra@albany.edu>
2015-09-18 01:43:31 -0400

    Swiftris iOS project

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -9,36 +9,16 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+
+    required init(coder aDecoder: NSCoder){
+        fatalError("NSCoder not supported")
     }
     
-    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch in (touches as! Set<UITouch>) {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+    override init(size: CGSize){
+        super.init(size:size)
     }
-   
+    
+    anchorPoint = CGPoint(x:0, y:0)
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
